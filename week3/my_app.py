0965f58e86741de33607ef22fa5dda8a8bfa0e12
Roswald9 <145515196+Roswald9@users.noreply.github.com>
2026-09-15 10:13:09 -0400

    App Updated

--- week3/my_app.py
+++ week3/my_app.py
@@ -16,3 +16,12 @@ import pandas as pd
 st.subheader("Exploring Our Dataset")
 
 df = pd.read_csv("data/sample_data.csv")
+
+st.write("Here's our data:")
+st.dataframe(df)
+
+city =st.selectbox("Select a city", df["City"].unique())
+st.write(f"People in {city}:")
+df[df["City"] == city]
+
+st.bar_chart(df["Salary"])
